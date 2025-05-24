25e7af8d2f213b820628a94da282ad4a708f728d
Linda Medeiros <medeiros.lindinhaa@gmail.com>
2025-05-24 11:54:26 -0300

    Add clubs router

--- clivro/main.py
+++ clivro/main.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from clivro.database import engine, Base, get_db
 
 from users.models import User as UserModel
+from clubs.controllers import router as clubs_router
 from users.controllers import router as users_router
 from users.schemas import UserOut
 from users.utils import get_current_user
@@ -15,6 +16,7 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(users_router, prefix="/users", tags=["users"])
+app.include_router(clubs_router, prefix="/clubs", tags=["clubs"])
 
 
 @app.get("/", response_model=UserOut, status_code=status.HTTP_200_OK)
