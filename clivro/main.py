751d4a8f248d4864b8e6efde1e0c937609dcf191
Linda Medeiros <medeiros.lindinhaa@gmail.com>
2025-06-08 22:52:44 -0300

    Add members providers

--- clivro/main.py
+++ clivro/main.py
@@ -6,11 +6,13 @@ from sqlalchemy.orm import Session
 
 from clivro.database import engine, Base, get_db
 
-from users.models import User as UserModel
+# from users.models import User as UserModel
+# from users.schemas import UserOut
+# from users.utils import get_current_user
+
 from clubs.controllers import router as clubs_router
+from members.controllers import router as members_router
 from users.controllers import router as users_router
-from users.schemas import UserOut
-from users.utils import get_current_user
 
 app = FastAPI()
 
@@ -26,10 +28,11 @@ app.add_middleware(
 
 app.include_router(users_router, prefix="/users", tags=["users"])
 app.include_router(clubs_router, prefix="/clubs", tags=["clubs"])
+app.include_router(members_router, prefix="/clubs/{club_id}/members", tags=["members"])
 
 
-@app.get("/", response_model=UserOut, status_code=status.HTTP_200_OK)
-async def get_user(user: Annotated[UserModel, Depends(get_current_user)]):
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
-    return user
+# @app.get("/", response_model=UserOut, status_code=status.HTTP_200_OK)
+# async def get_user(user: Annotated[UserModel, Depends(get_current_user)]):
+#     if user is None:
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
+#     return user
