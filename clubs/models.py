276e2f87a8523092e9f82bfd711ca448b2b9d9b9
Linda Medeiros <medeiros.lindinhaa@gmail.com>
2025-06-08 22:51:17 -0300

    Add relationshio to members

--- clubs/models.py
+++ clubs/models.py
@@ -1,16 +1,22 @@
 from datetime import datetime
 
+from sqlalchemy.orm import relationship
+
 from clivro.database import Base
 from sqlalchemy import UUID, Column, String, DateTime
 from uuid_extensions import uuid7
+
+from members.models import Member
 
 
 class Club(Base):
     __tablename__ = 'clubs'
 
-    id = Column(UUID, primary_key=True, default=uuid7())
+    id = Column(UUID, primary_key=True, default=uuid7)
     name = Column(String, nullable=False, unique=True)
     description = Column(String, nullable=True)
     owner_id = Column(String, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column(DateTime)
+
+    members = relationship("Member", back_populates="clubs")
--- users/models.py
+++ users/models.py
@@ -1,17 +1,23 @@
 from datetime import datetime
 
+from sqlalchemy.orm import relationship
+
 from clivro.database import Base
 from sqlalchemy import UUID, Column, String, DateTime
 from uuid_extensions import uuid7
+
+from members.models import Member
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(UUID, primary_key=True, default=uuid7())
+    id = Column(UUID, primary_key=True, default=uuid7)
     name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     username = Column(String, nullable=False, unique=True)
     hashed_password = Column(String, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column(DateTime)
+
+    members = relationship("Member", back_populates='users')
